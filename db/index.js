247300b5ea5202564e6fbfe494289071d42c7339
XuHan <1178263698@qq.com>
2023-11-16 16:20:54 +0800

    feat:env

--- db/index.js
+++ db/index.js
@@ -1,10 +1,13 @@
+
+require('dotenv').config()
+console.log(process.env.host)
 //导入mysql数据库
 const mysql = require("mysql");
 //创建数据库连接池
 const db = mysql.createPool({
-  host: "localhost",
-  user: "modu-cockpit",
-  password: "123456",
+  host: process.env.host,
+  user: process.env.db_user,
+  password: process.env.db_password,
   database: "modu-cockpit",
 });
 //向外暴露数据库
